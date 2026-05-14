ad91448d7536a33d73aea4529b92953201415bf0
ah0048 <ahmedharhash0048@gmail.com>
2026-05-14 16:11:59 +0300

    finished Garbage Collection and Dispose Pattern

---
+++ Garbage Collection and Dispose Pattern/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ConsoleApp1
+{
+    public class Customer : IDisposable
+    {
+
+        private StringReader _reader;
+        bool disposed = false;
+        public Customer(string s)
+
+        {
+
+            this._reader = new StringReader(s);
+
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed) return;
+
+            if (disposing)
+            {
+                _reader?.Dispose();
+            }
+
+            disposed = true;
+        }
+
+        ~Customer()
+        {
+            Dispose(false);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("playing with GC and Dispose pattern");
+        }
+    }
+}
